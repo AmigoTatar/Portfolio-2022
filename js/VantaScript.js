0e3b7b2b13cb0a0b9575785f711fcd3e79db369b
AmigoTatar <124tbtm@gmail.com>
2022-10-21 12:55:12 +0500

    2022 okt  21

---
+++ js/VantaScript.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+  el: ".wrapper_header",
+    mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xe8d042,
+  backgroundColor: 0x0,
+  points: 16.00,
+  maxDistance: 23.00,
+  spacing: 20.00
+})
---
+++ js/button.js
@@ -0,0 +1,5 @@
+function buttonFunc(){
+document.getElementById('#work');
+	work.style.display = 'flex';
+	hidenBut.style.display =  'none';
+};
